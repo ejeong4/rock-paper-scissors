7876bf3ee346332e630f5293239b9edbecd16864
ejeong4 <mlej530@gmail.com>
2022-07-09 23:09:46 -0400

    Add icons and revise styling

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const playBtn = document.querySelector('.play-btn');
+const playBtn = document.querySelector('#start-play');
 const body = document.querySelector('body');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
 
 function mouse(e) {
+    console.log(e.target.className);
+    const target = e.target.className;
+    console.log(target + '-hover');
     if (e.type == "mouseover") {
-        this.classList.remove('play-btn');
-        this.classList.add('play-btn-hover');
+        this.classList.remove(`play-btn`);
+        this.classList.add(`play-btn-hover`);
         e.stopPropagation();
     } else if (e.type == "mouseout") {
         this.classList.remove('play-btn-hover');
@@ -21,10 +27,17 @@ function mouse(e) {
     }
 }
 
+
+
 playBtn.addEventListener('mouseover', mouse);
 playBtn.addEventListener('mouseout', mouse);
 playBtn.addEventListener('mousedown', mouse);
 playBtn.addEventListener('mouseup', mouse);
+
+
+
+
+
 
 
 
