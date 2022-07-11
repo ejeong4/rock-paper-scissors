b2790bf5416ee5a202d48534d2e67c2204b500c4
ejeong4 <mlej530@gmail.com>
2022-07-11 11:57:41 -0400

    Remove unnecessary code and adjust sizing

--- js/script.js
+++ js/script.js
@@ -1,35 +1,16 @@
 const playBtn = document.querySelector('#start-play');
-const body = document.querySelector('body');
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('.scissors');
-
-
 
 function mouse(e) {
-    console.log(e.target.className);
-    const target = e.target.className;
-    console.log(target + '-hover');
-    if (e.type == "mouseover") {
-        this.classList.remove(`play-btn`);
+    if (e.type == 'mouseover') {
         this.classList.add(`play-btn-hover`);
-        e.stopPropagation();
-    } else if (e.type == "mouseout") {
+    } else if (e.type == 'mouseout') {
         this.classList.remove('play-btn-hover');
-        this.classList.add('play-btn');
-        e.stopPropagation();
     } else if (e.type == "mousedown") {
-        this.classList.remove('play-btn-hover');
         this.classList.add('play-btn-click');
-        e.stopPropagation();
     } else if (e.type == "mouseup") {
         this.classList.remove('play-btn-click');
-        this.classList.add('play-btn-hover');
-        e.stopPropagation();
     }
 }
-
-
 
 playBtn.addEventListener('mouseover', mouse);
 playBtn.addEventListener('mouseout', mouse);
